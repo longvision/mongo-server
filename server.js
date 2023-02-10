3456ebca15e0c7eac15ca5a694bbdf93e3018733
Ricardo Naoki H <r.n.hori@gmail.com>
2023-02-10 06:59:56 -0800

    Update server.js

--- server.js
+++ server.js
@@ -10,35 +10,38 @@ const URL = process.env.MONGO_URL;
 
 const app = express() //creating the express server
 
-//create a port for the server to listen to
-const PORT = 3001
+
+const PORT = 3001 //create a port for the server to listen to
 
 app.use(express.json()) //this is a middleware that allows us to parse the body of the request
 
-app.listen(PORT, () => {
+app.listen(PORT, () => { //server listens on defined port (3001) 
     console.log(`Server is running on port ${PORT}`)
 })
 
 connection(URL) //calling the connection function from the dbConnection.js file
 
-//lets create the database conection using mongodb
-app.post('/create', async (req, res) => { //<---async keyword is used here
+
+app.post('/create', async (req, res) => { //<---async keyword is used here.
     //this is the body of the POST REST API request.
     let body = req.body
+    
     console.log(body)
 
     // here we are using the user schema to create a new user in the database
     // the await keyword is used to wait for the promise to resolve before continuing the execution of the code
     // when using await, we need to use the async keyword in the function declaration
 
-    // await user.create(body).then(result => {
+    // await user.create(body).then(result => { //<---await keyword is used here to process the async call.
     //     res.send({ status: 200, message: "User created successfully", data: result })
     // }).catch(err => {
     //     res.send({ status: 500, message: "Error creating user", data: err })
     // })
-
+    
+    //create the user mongoose object in the defined schema format.
     const newUser = new User(body)
-
+    
+    //saving the user created and sending the response to the client.
     newUser.save((err, result) => {
         if (err) {
             res.send({ status: 500, message: "Error creating user", data: err })
@@ -47,35 +50,9 @@ app.post('/create', async (req, res) => { //<---async keyword is used here
 
     })
 
-
-    //lets create the curl request now:
-
-    //     curl - X POST \
-    //     http://localhost:3000/users \
-    //     -H 'Content-Type: application/json' \
-    //     -d '{
-    //     "name": "John Doe",
-    //         "email": "johndoe@example.com",
-    //             "password": "secretpassword"
-    // }'
-
-
-
 })
 
 
 
-//we need to create a new mongo schema, which is a blueprint for the data we want to store in the database
-// we need to create a new folder called models, and create a new file called user.js
-
-
-//where did you get this code above?
-//did you try using mongoose?
-//offical website
-//npm install express
-//let install mongoose to interact with mongodb
-//npm install mongoose
-//Done
-// https://www.mongodb.com/developer/languages/javascript/getting-started-with-mongodb-and-mongoose/
 
 
